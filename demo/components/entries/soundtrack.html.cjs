95550694d97bef8a1dd80985c8c5bc4e37ce661e
Luis Brayan <luis.bs.9004@gmail.com>
2025-02-27 14:25:07 -0600

    refactor: clean up on demo

--- demo/components/entries/soundtrack.html.cjs
+++ demo/components/entries/soundtrack.html.cjs
@@ -9,17 +9,17 @@ const { ATTRS, HEADER_ATTRS, LINKS_ATTRS, MEDIA_ATTRS } = require('./commons/mus
  * @returns {Iterable<[HtmlRenderer, MetadataField, unknown]>}
  */
 function* iterate(row, attrs, parentEl, divClass) {
-  if (!Obj.includes(row, attrs)) return;
+    if (!Obj.includes(row, attrs)) return;
 
-  const divEl = parentEl.div(divClass);
-  for (const [key, value] of Obj.flattenEntries(row, attrs)) {
-    yield [divEl, ATTRS.get(key) || {}, value];
-  }
+    const divEl = parentEl.div(divClass);
+    for (const [key, value] of Obj.flattenEntries(row, attrs)) {
+        yield [divEl, ATTRS.get(key) || {}, value];
+    }
 }
 
 /** @param {MusicMetadata} row */
 function innerCls(row) {
-  return ['vault-soundtrack', row.link || row.links ? 'with-links' : ''];
+    return ['vault-soundtrack', row.link || row.links ? 'with-links' : ''];
 }
 
 /**
@@ -27,34 +27,39 @@ function innerCls(row) {
  * @param {CodeblockContext} context
  */
 module.exports.render = async function (input, context) {
-  /** @type {MusicMetadata[]} */
-  const data = Arr.wrap(input);
-  // console.log({ input, data });
+    /** @type {MusicMetadata[]} */
+    const data = Arr.wrap(input);
+    // console.log({ input, data });
 
-  const rootEl = new HtmlRenderer();
-  for (const row of data) {
-    const cardEl = rootEl.div(innerCls(row));
+    const rootEl = new HtmlRenderer();
+    for (const row of data) {
+        const cardEl = rootEl.div(innerCls(row));
 
-    // soundtrack-header
-    for (const [divEl, attr, value] of iterate(row, HEADER_ATTRS, cardEl, 'soundtrack-header')) {
-      divEl.el(attr.tag, value || attr.fallback || '');
+        // soundtrack-header
+        for (const [divEl, attr, value] of iterate(
+            row,
+            HEADER_ATTRS,
+            cardEl,
+            'soundtrack-header',
+        )) {
+            divEl.el(attr.tag, value || attr.fallback || '');
+        }
+
+        // soundtrack-links
+        for (const [divEl, attr, value] of iterate(row, LINKS_ATTRS, cardEl, 'soundtrack-links')) {
+            // const service = SERVICES.getService(key);
+            // if (service) {
+            //   linksEl.link(service.getShareUrl(value), attr.text);
+            // } else {
+            divEl.link(value, URI.getURLDomain(value) || attr.text);
+        }
+
+        // soundtrack-media
+        for (const [divEl, attr, value] of iterate(row, MEDIA_ATTRS, cardEl, 'soundtrack-media')) {
+            const service = SERVICES.getService(key);
+            divEl.iframe(service.getEmbedUrl(value));
+        }
     }
 
-    // soundtrack-links
-    for (const [divEl, attr, value] of iterate(row, LINKS_ATTRS, cardEl, 'soundtrack-links')) {
-      // const service = SERVICES.getService(key);
-      // if (service) {
-      //   linksEl.link(service.getShareUrl(value), attr.text);
-      // } else {
-      divEl.link(value, URI.getURLDomain(value) || attr.text);
-    }
-
-    // soundtrack-media
-    for (const [divEl, attr, value] of iterate(row, MEDIA_ATTRS, cardEl, 'soundtrack-media')) {
-      const service = SERVICES.getService(key);
-      divEl.iframe(service.getEmbedUrl(value));
-    }
-  }
-
-  return rootEl.getContent();
+    return rootEl.getContent();
 };
